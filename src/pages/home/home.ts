4fa0ef5ce2e7db5b3b5ffc62714edb9b87047405
geektechie <geektechie712@gmail.com>
2017-11-07 11:33:18 +0530

    ionic app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import { RecipePage } from '../recipe/recipe';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	recipes: any;
+  constructor(public navCtrl: NavController, public http:Http) {
+
+
+   this.http.get('http://localhost:49919/api/ChefyTricksAPI/recipeList').subscribe(data => {
+        var response = data.json();
+      
+        this.recipes=response.recipes;
+       
+    });
 
   }
 
+
+
+details(r){
+	console.log(r);
+  this.navCtrl.push(RecipePage,{details:r});
+}
 }
